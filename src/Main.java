44c7f87a736a051e7a8e9c0f2622faaf9873cfa0
herreang000 <herreang000@warren.k12.in.us>
2025-01-15 11:15:28 -0500

    final

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        java.util.Arrays.sort(numbers);
+        System.out.println("min-" + numbers[0] + " max-" + numbers[numbers.length - 1]);
+        int sum = 0;
+
+        for (int num : numbers) {
+            sum += num;
+        }
+        System.out.println("Sum: " + sum);
+
+        int average = 0;
+        for (int num : numbers) {
+            average = sum / numbers.length;
+        }
+        System.out.println("Average: " + average);
+
+
+    }
 }
